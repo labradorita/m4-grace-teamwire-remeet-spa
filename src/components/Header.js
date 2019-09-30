706f6351391a8de7356191acd444e34cc312a5a1
Sara Martín <martingutierrezsara@gmail.com>
2019-09-30 21:06:38 +0200

    create header component and scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
 import '../stylesheets/Header.scss'
 
-const Header = props => {
+const Header = () => {
     return (
         <header className="header">
             <div className="header__logo">
                 <img src="" alt="logo-Remeet" className="header__img" />
             </div>
-            <nav className="header__nav">
-                <ul>
-                    <li>About Remeet</li>
-                    <li>Help</li>
+            <nav>
+                <ul className="header__nav">
+                    <li className="header__element">About Remeet</li>
+                    <li className="header__element">Help</li>
                 </ul>
             </nav>
         </header>
