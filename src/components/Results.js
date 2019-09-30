5078b5e5d9bce4984821943fec139e86b13b00a7
Samantha Cancino <samanthacancino@gmail.com>
2019-10-01 00:48:59 +0200

    Results List styled, next step, integration in Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ResultsList from "./ResultsList";
 import ResultsButton from "./ResultsButton";
+import Return from "./Return";
 import "../stylesheets/layouts/Results.scss";
 
 const Results = () => {
   return (
-    <div className="results__list__container">
-      <ul className="results__list">
-        <ResultsList />
-      </ul>
-      <ResultsButton />
-    </div>
+    <React.Fragment>
+      <div className="results__container">
+        <ul className="results__list">
+          <ResultsList />
+        </ul>
+        <ResultsButton />
+      </div>
+      <Return />
+    </React.Fragment>
   );
 };
 
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,10 +1,35 @@
 import React from "react";
+import "../stylesheets/layouts/Results.scss";
 
 const ResultsList = () => {
   return (
     <li className="results__item">
-      <p>RESULTS LIST</p>
-      <p>LISISIISSIISISISISISISI</p>
+      <div>
+        <div className="results__item__city">
+          <div>
+            <h3>London</h3>
+            <small>Gatwick Airport</small>
+          </div>
+          <div className="results__item__totalprice">
+            <h2>1.250€</h2>
+            <small>TOTAL PRICE</small>
+          </div>
+        </div>
+        <div className="results__item__partial">
+          <div className="results__item__participants">
+            <h4> 2 Participants</h4>
+            <small>From Madrid</small>
+          </div>
+          <p className="results__item__price">950€</p>
+        </div>
+        <div className="results__item__partial">
+          <div className="results__item__participants">
+            <h4> 1 Participant</h4>
+            <small>From Geneva</small>
+          </div>
+          <p className="results__item__price">300€</p>
+        </div>
+      </div>
     </li>
   );
 };
