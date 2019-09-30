120b08e06327c79d1c6f3153538f5a376c63112d
Samantha Cancino <samanthacancino@gmail.com>
2019-09-30 22:13:34 +0200

    First commit ResultsList

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ResultsList from "./ResultsList";
 
 const Results = () => {
   return (
     <div className="header">
       <p>RESULTS</p>
+      <ResultsList />
       <p>RESUSUSUSUSUSUSUUSU</p>
     </div>
   );
---
+++ src/components/ResultsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ResultsList = () => {
+  return (
+    <div className="header">
+      <p>RESULTS LIST</p>
+      <p>LISISIISSIISISISISISISI</p>
+    </div>
+  );
+};
+
+export default ResultsList;
