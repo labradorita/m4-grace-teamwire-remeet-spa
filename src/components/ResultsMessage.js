7e59c703cbb6e51a0887571e052f559b2f8f4073
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-01 17:10:57 +0200

    locate results message

--- src/components/ResultsMessage.js
+++ src/components/ResultsMessage.js
@@ -6,11 +6,13 @@ const ResultsMessage = () => {
     <div className="results-message">
       <h2 className="results-message__message">We find the best Remeet in</h2>
       <p className="results-message__city">{`Madrid`}</p>
-      <img
-        src={`https://cdn.civitatis.com/espana/madrid/guia/madrid.jpg`}
-        alt="Madrid, Edificio Metrópolis"
-        className="results-message__image"
-      />
+      <div className="results-message__image-container">
+        <img
+          src={`https://cdn.civitatis.com/espana/madrid/guia/madrid.jpg`}
+          alt="Madrid, Edificio Metrópolis"
+          className="results-message__image"
+        />
+      </div>
     </div>
   );
 };
