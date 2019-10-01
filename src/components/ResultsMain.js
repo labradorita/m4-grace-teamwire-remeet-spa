8ed388c002d0ff7f19fec6a1fd35fef0e8efa764
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-01 12:32:54 +0200

    add results_main styles 1.2

--- src/components/ResultsMain.js
+++ src/components/ResultsMain.js
@@ -9,34 +9,44 @@ const ResultsMain = () => {
         alt="Madrid, Edificio Metrópolis"
         className="results-main__image"
       />
-      <div className="results-main__city-airport-price">
-        <h2 className="results-main__city">City</h2>
-        <p className="results-main__airport">Airport</p>
-        <p className="results-main__price">0000 €</p>
-      </div>
-      <div className="results-main__book-column">
-        <button className="results-main__book-button">BOOK</button>
-        <p className="results-main__book-column--total-price">TOTAL PRICE</p>
+      <div className="results-main__booking-section">
+        <div className="results-main__city-airport-price">
+          <h2 className="results-main__city">City</h2>
+          <p className="results-main__airport">Airport</p>
+          <p className="results-main__price">0000 €</p>
+        </div>
+        <div className="results-main__book-column">
+          <button className="results-main__book-button">BOOK</button>
+          <p className="results-main__total-price">TOTAL PRICE</p>
+        </div>
       </div>
       <div className="results-main__participants-info">
         <ul className="results-main__participants-list">
           <li className="results-main__participants-item">
-            <h4 className="results-main__participants-number">
-              {`Nº`} participants
-            </h4>
-            <p className="results-main__participants-location">
-              From {`location`}
-            </p>
-            <number>{`price for location €`}</number>
+            <div className="results-main__participants--left-column">
+              <h4 className="results-main__participants-number">
+                {`Nº`} participants
+              </h4>
+              <p className="results-main__participants-location">
+                From {`location`}
+              </p>
+            </div>
+            <div className="results-main__participants--right-column">
+              <number className="results-main__participants-price">{`000 €`}</number>
+            </div>
           </li>
           <li className="results-main__participants-item">
-            <h4 className="results-main__participants-number">
-              {`Nº`} participants
-            </h4>
-            <p className="results-main__participants-location">
-              From {`location`}
-            </p>
-            <number>{`price for location €`}</number>
+            <div className="results-main__participants--left-column">
+              <h4 className="results-main__participants-number">
+                {`Nº`} participants
+              </h4>
+              <p className="results-main__participants-location">
+                From {`location`}
+              </p>
+            </div>
+            <div className="results-main__participants--right-column">
+              <number className="results-main__participants-price">{`000 €`}</number>
+            </div>
           </li>
         </ul>
       </div>
