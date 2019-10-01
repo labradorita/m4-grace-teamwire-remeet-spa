b2962534cb6073f7e94590495908544ff35109f8
Mar Prieto <mar.pnieto@gmail.com>
2019-10-01 10:30:29 +0200

    starting java

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,24 @@ import React from "react";
 import SearchParticipants from "./SearchParticipants";
 import "../stylesheets/layouts/App.scss";
 import SearchDate from './SearchDate';
+import getDataFromServer from './getDataFromServer';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      employees: [],
+      offices: [],
+      airports: [],
       dateIn: "", // AAAA-MM-DD
       dateOut: "", // AAAA-MM-DD
     }
     this.handleDateIn = this.handleDateIn.bind(this);
     this.handleDateOut = this.handleDateOut.bind(this);
+
+  }
+
+  componentDidMount() {
 
   }
 
