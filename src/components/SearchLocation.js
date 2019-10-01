dbe9b89c47baf71ab9a2e3dd68fbe570d29ff52c
Raquel MM <mendizraquel@gmail.com>
2019-10-01 12:48:18 +0200

    Solve console errors

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,45 +1,35 @@
-import React from 'react';
-import '../stylesheets/SearchLocation.scss'
-
-
-
+import React from "react";
+import "../stylesheets/SearchLocation.scss";
 
 const SearchLocation = () => {
-    return (
-        <div class="searchlocation">
-            <p className="searchlocation__title">Meeting Point Availables</p>
-            <div>
-                <i class="fas fa-chevron-up" />
-            </div>
-            <div className="searchlocation__container">
-                <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">Munich</h4>
-                        <p className="searchlocation__airport">Munchen Airport</p>
-                       
-                    </li>
-
-                </ul>
-                <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">Madrid</h4>
-                        <p className="searchlocation__airport">Adolfo Suarez</p>
-
-                    </li>
-
-                </ul>
-                <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">London</h4>
-                        <p className="searchlocation__airport">Gatwick Airport</p>
-
-                    </li>
-
-                </ul>
-            </div>
-
-        </div>
-    );
-}
+  return (
+    <div className="searchlocation">
+      <p className="searchlocation__title">Meeting Point Availables</p>
+      <div>
+        <i className="fas fa-chevron-up" />
+      </div>
+      <div className="searchlocation__container">
+        <ul className="searchlocation__list">
+          <li>
+            <h4 className="searchlocation__city">Munich</h4>
+            <p className="searchlocation__airport">Munchen Airport</p>
+          </li>
+        </ul>
+        <ul className="searchlocation__list">
+          <li>
+            <h4 className="searchlocation__city">Madrid</h4>
+            <p className="searchlocation__airport">Adolfo Suarez</p>
+          </li>
+        </ul>
+        <ul className="searchlocation__list">
+          <li>
+            <h4 className="searchlocation__city">London</h4>
+            <p className="searchlocation__airport">Gatwick Airport</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default SearchLocation;
