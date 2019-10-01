0766488313f08981210b8963ce4f4676fcd6a434
Sara Martín <martingutierrezsara@gmail.com>
2019-10-01 14:33:40 +0200

    finish scss

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,37 +1,51 @@
 import React from 'react';
-import '../stylesheets/SearchLocation.scss'
+import '../stylesheets/layouts/SearchLocation.scss'
 
 
 
 
 const SearchLocation = () => {
     return (
-        <div class="searchlocation">
-            <p className="searchlocation__title">Meeting Point Availables</p>
-            <div>
-                <i class="fas fa-chevron-up" />
+        <div className="searchlocation">
+            <div className="searchlocation__main">
+                <p className="searchlocation__title">Meeting Point Availables</p>
+                <i className="fas fa-chevron-up" />
             </div>
             <div className="searchlocation__container">
                 <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">Munich</h4>
-                        <p className="searchlocation__airport">Munchen Airport</p>
-                       
-                    </li>
-
-                </ul>
-                <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">Madrid</h4>
-                        <p className="searchlocation__airport">Adolfo Suarez</p>
+                    <li className="searchlocation__list__element">
+                        <div>
+                            <h4 className="searchlocation__list__city">Munich</h4>
+                            <p className="searchlocation__list__airport">Munchen Airport</p>
+                        </div>
+                        <div className="searchlocation__list__icon">
+                            <i className="fas fa-plane"></i>
+                        </div>
 
                     </li>
 
                 </ul>
                 <ul className="searchlocation__list">
-                    <li>
-                        <h4 className="searchlocation__city">London</h4>
-                        <p className="searchlocation__airport">Gatwick Airport</p>
+                    <li className="searchlocation__list__element">
+                        <div>
+                            <h4 className="searchlocation__list__city">Madrid</h4>
+                            <p className="searchlocation__list__airport">Adolfo Suarez</p>
+                        </div>
+                        <div className="searchlocation__list__icon">
+                            <i className="fas fa-plane"></i>
+                        </div>
+                    </li>
+
+                </ul>
+                <ul className="searchlocation__list">
+                    <li className="searchlocation__list__element">
+                        <div>
+                            <h4 className="searchlocation__list__city">London</h4>
+                            <p className="searchlocation__list__airport">Gatwick Airport</p>
+                        </div>
+                        <div className="searchlocation__list__icon">
+                            <i className="fas fa-plane"></i>
+                        </div>
 
                     </li>
 
