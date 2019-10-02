5ee7a8e61e17a20c0d44cfee07a0191b7fb769f6
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-02 13:45:08 +0200

    add style general details

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../stylesheets/layouts/App.scss";
 import getDataFromServer from "./getDataFromServer";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Results from "./Results";
@@ -77,7 +77,6 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Header />
-        <Link to="/results">Results</Link>
         <Switch>
           <Route
             exact
