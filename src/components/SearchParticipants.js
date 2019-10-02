6e1c2b9fb1925c8359e01a0aabb8ebc275148a6e
Samantha Cancino <samanthacancino@gmail.com>
2019-10-02 17:28:38 +0200

    fix participants

--- src/components/SearchParticipants.js
+++ src/components/SearchParticipants.js
@@ -6,32 +6,31 @@ import face from "../images/face.png";
 const SearchParticipants = props => {
   const { employees, getAirportNameByCode } = props;
   return (
-    <div className="SearchParticipants">
-      <h2 className="SearchParticipants__mainTitle">select participants</h2>
-      <section className="SearchParticipants__container">
-        <ul className="SearchParticipants__list">
+    <div className="search-participants">
+      <h2 className="search-participants__title">select participants</h2>
+      <section className="search-participants__container">
+        <ul className="search-participants__list">
           {employees.map(employee => (
-            <li className="SearchParticipants__participant">
-              <img
-                className="SearchParticipants__img"
-                src={face}
-                alt="participant face"
-              />
-              <div className="SearchParticipants__info">
-                <h4 className="SearchParticipants__info__name">
-                  {employee.name}
-                </h4>
-                <p className="SearchParticipants__info__city">
-                  {getAirportNameByCode(employee.airportCode)}
-                </p>
+            <li className="search-participants__participant">
+              <div className="search-participants__data">
+                <img
+                  className="search-participants__image"
+                  src={face}
+                  alt="participant face"
+                />
+                <div className="search-participants__info">
+                  <h4 className="search-participants__info-name">
+                    {employee.name}
+                  </h4>
+                  <p className="search-participants__info-city">
+                    {getAirportNameByCode(employee.airportCode)}
+                  </p>
+                </div>
               </div>
-
-              <input
-                className="SearchParticipants__checkbox"
-                type="checkbox"
-                defaultChecked="checked"
-                //onChange =
-              ></input>
+              <label htmlFor="checkbox" className="container">
+                <input id="checkbox" type="checkbox" defaultChecked="checked" />
+                <span className="checkmark"></span>
+              </label>
             </li>
           ))}
         </ul>
