34657dafeb2f1a566fadf246c731d2193b1ca324
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-02 12:25:11 +0200

    fix search location styles details

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,59 +1,50 @@
-import React from 'react';
-import '../stylesheets/layouts/SearchLocation.scss'
-
-
-
+import React from "react";
+import "../stylesheets/layouts/SearchLocation.scss";
 
 const SearchLocation = () => {
-    return (
-        <div className="searchlocation">
-            <div className="searchlocation__main">
-                <p className="searchlocation__title">Meeting Point Availables</p>
-                <i className="fas fa-chevron-up" />
+  return (
+    <div className="search-location">
+      <div className="search-location__main">
+        <p className="search-location__title">Meeting Point Availables</p>
+        <i className="fas fa-chevron-up" />
+      </div>
+      <div className="search-location__container">
+        <ul className="search-location__list">
+          <li className="search-location__list__element">
+            <div>
+              <h4 className="search-location__list__city">Munich</h4>
+              <p className="search-location__list__airport">Munchen Airport</p>
             </div>
-            <div className="searchlocation__container">
-                <ul className="searchlocation__list">
-                    <li className="searchlocation__list__element">
-                        <div>
-                            <h4 className="searchlocation__list__city">Munich</h4>
-                            <p className="searchlocation__list__airport">Munchen Airport</p>
-                        </div>
-                        <div className="searchlocation__list__icon">
-                            <i className="fas fa-plane"></i>
-                        </div>
-
-                    </li>
-
-                </ul>
-                <ul className="searchlocation__list">
-                    <li className="searchlocation__list__element">
-                        <div>
-                            <h4 className="searchlocation__list__city">Madrid</h4>
-                            <p className="searchlocation__list__airport">Adolfo Suarez</p>
-                        </div>
-                        <div className="searchlocation__list__icon">
-                            <i className="fas fa-plane"></i>
-                        </div>
-                    </li>
-
-                </ul>
-                <ul className="searchlocation__list">
-                    <li className="searchlocation__list__element">
-                        <div>
-                            <h4 className="searchlocation__list__city">London</h4>
-                            <p className="searchlocation__list__airport">Gatwick Airport</p>
-                        </div>
-                        <div className="searchlocation__list__icon">
-                            <i className="fas fa-plane"></i>
-                        </div>
-
-                    </li>
-
-                </ul>
+            <div className="search-location__list__icon">
+              <i className="fas fa-plane"></i>
             </div>
-
-        </div>
-    );
-}
+          </li>
+        </ul>
+        <ul className="search-location__list">
+          <li className="search-location__list__element">
+            <div>
+              <h4 className="search-location__list__city">Madrid</h4>
+              <p className="search-location__list__airport">Adolfo Suarez</p>
+            </div>
+            <div className="search-location__list__icon">
+              <i className="fas fa-plane"></i>
+            </div>
+          </li>
+        </ul>
+        <ul className="search-location__list">
+          <li className="search-location__list__element">
+            <div>
+              <h4 className="search-location__list__city">London</h4>
+              <p className="search-location__list__airport">Gatwick Airport</p>
+            </div>
+            <div className="search-location__list__icon">
+              <i className="fas fa-plane"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default SearchLocation;
