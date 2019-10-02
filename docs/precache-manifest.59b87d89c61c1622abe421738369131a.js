80113d40fec376bc8dd34a266bb401542a374759
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-02 18:08:57 +0200

    build docs folder

---
+++ docs/precache-manifest.59b87d89c61c1622abe421738369131a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e1e17ee89db4fefddf7fdc4514d25b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c58c8a08857eae261834",
+    "url": "./static/css/main.e105df3e.chunk.css"
+  },
+  {
+    "revision": "e5e5ec5369f6c9406ba2",
+    "url": "./static/js/2.eb83d85f.chunk.js"
+  },
+  {
+    "revision": "c58c8a08857eae261834",
+    "url": "./static/js/main.4072b32c.chunk.js"
+  },
+  {
+    "revision": "464e172959119b21e3c0",
+    "url": "./static/js/runtime-main.de052180.js"
+  },
+  {
+    "revision": "44222db4946e4d619d09cc9f994592ca",
+    "url": "./static/media/Illustration.44222db4.png"
+  },
+  {
+    "revision": "68e7ec97ccd283f873a7cbc52f458353",
+    "url": "./static/media/bubble.68e7ec97.png"
+  },
+  {
+    "revision": "ad158d24981cd9a89c12eb97b19cd540",
+    "url": "./static/media/face.ad158d24.png"
+  },
+  {
+    "revision": "0a64ef00861ca685c93c6405bc6a69db",
+    "url": "./static/media/madrid-circle.0a64ef00.jpg"
+  },
+  {
+    "revision": "2d7de25cb9111f43f482e770e37a9ef1",
+    "url": "./static/media/result-city.2d7de25c.png"
+  }
+]);
