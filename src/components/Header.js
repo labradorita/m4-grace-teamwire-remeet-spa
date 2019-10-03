84888d120481e998e175533fab205fa044769fdc
Samantha Cancino <samanthacancino@gmail.com>
2019-10-03 16:57:10 +0200

    link in logo and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
-import '../stylesheets/layouts/Header.scss'
-import Logo from '../images/Logo.png'
+import "../stylesheets/layouts/Header.scss";
+import Logo from "../images/Logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="header__logo">
-                <img src={Logo} alt="logo-Remeet" className="header__img" />
-            </div>
-            <nav>
-                <ul className="header__nav">
-                    <li className="header__element">About Remeet</li>
-                    <li className="header__element">Help</li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/">
+          <img src={Logo} alt="logo-Remeet" className="header__img" />
+        </Link>
+      </div>
+      <nav>
+        <ul className="header__nav">
+          <li className="header__element">About Remeet</li>
+          <li className="header__element">Help</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
