70bda12a4a10011241d37b9c128f7c818497d5ac
Samantha Cancino <samanthacancino@gmail.com>
2019-10-03 13:43:22 +0200

    finish result-message

--- src/components/ResultsMessage.js
+++ src/components/ResultsMessage.js
@@ -9,7 +9,9 @@ const ResultsMessage = props => {
     return (
       <div className="results-message">
         <h2 className="results-message__message">We find the best Remeet in</h2>
-        <p className="results-message__city">{`Madrid`}</p>
+        <p className="results-message__city">
+          {getAirportPropByCode(element.airportTo)("name")}
+        </p>
         <div className="results-message__image-container">
           <img
             src={getAirportPropByCode(element.airportTo)("imageThumb")}
