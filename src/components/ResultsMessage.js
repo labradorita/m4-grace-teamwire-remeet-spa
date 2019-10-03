f8d6f302b6114d54770ea91819ef4b575277e7b5
Samantha Cancino <samanthacancino@gmail.com>
2019-10-03 13:40:09 +0200

    fix

--- src/components/ResultsMessage.js
+++ src/components/ResultsMessage.js
@@ -2,26 +2,25 @@ import React from "react";
 import "../stylesheets/layouts/ResultsMessage.scss";
 import madridCircle from "../images/madrid-circle.jpg";
 
-const ResultsMessage = (props) => {
-  if (props.composeList !== []) {
-    const { element, getAirportPropByCode } = props
-    console.log(element)
+const ResultsMessage = props => {
+  if (props.element) {
+    const { element, getAirportPropByCode } = props;
+    console.log(element);
     return (
       <div className="results-message">
         <h2 className="results-message__message">We find the best Remeet in</h2>
         <p className="results-message__city">{`Madrid`}</p>
         <div className="results-message__image-container">
           <img
-            src={getAirportPropByCode(element)("imageThumb")}
+            src={getAirportPropByCode(element.airportTo)("imageThumb")}
             alt="Madrid, Edificio Metrópolis"
             className="results-message__image"
           />
         </div>
       </div>
-    )
-  }
-  else {
-    return ("Cargando...")
+    );
+  } else {
+    return "Cargando...";
   }
 };
 
